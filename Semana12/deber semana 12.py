fb090b5cdb0c7e1aa9bc7f6610fc95e40e27e404
Usuario <jf.alvercav@uea.edu.ec>
2024-09-06 00:42:33 -0500

    semana 12 primera version

---
+++ Semana12/deber semana 12.py
@@ -0,0 +1,150 @@
+matr_temp= [
+    [   # Ciudad 1
+        [   # Semana 1
+            {"Semana 1": "Zamora", "Lunes": 25},
+            {"Semana 1": "Zamora", "Martes": 30},
+            {"Semana 1": "Zamora", "Miercoles": 28},
+            {"Semana 1": "Zamora", "Jueves": 30},
+            {"Semana 1": "Zamora", "Viernes": 35},
+            {"Semana 1": "Zamora", "Sabado": 26},
+            {"Semana 1": "Zamora", "Domingo": 30}
+        ],
+        [   # Semana 2
+            {"Semana 2": "Zamora", "lunes": 30},
+            {"Semana 2": "Zamora", "Martes": 27},
+            {"Semana 2": "Zamora", "Miercoles": 35},
+            {"Semana 2": "Zamora", "Jueves": 40},
+            {"Semana 2": "Zamora", "Viernes": 26},
+            {"Semana 2": "Zamora", "Sabado": 30},
+            {"Semana 2": "Zamora", "Domingo": 25}
+
+        ],
+        [   # Semana 3
+            {"Semana 3": "Zamora", "lunes": 30},
+            {"Semana 3": "Zamora", "Martes": 30},
+            {"Semana 3": "Zamora", "Miercoles": 25},
+            {"Semana 3": "Zamora", "Jueves": 29},
+            {"Semana 3": "Zamora", "Viernes": 27},
+            {"Semana 3": "Zamora", "Sabado": 20},
+            {"Semana 3": "Zamora", "Domingo": 28}
+
+
+        ],
+        [   # Semana 4
+            {"Semana 4": "Zamora", "lunes": 25},
+            {"Semana 4": "Zamora", "Martes": 30},
+            {"Semana 4": "Zamora", "Miercoles": 28},
+            {"Semana 4": "Zamora", "Jueves": 26},
+            {"Semana 4": "Zamora", "Viernes": 24},
+            {"Semana 4": "Zamora", "Sabado": 29},
+            {"Semana 4": "Zamora", "Domingo": 25}
+
+        ]
+    ],
+    [   # Ciudad 2
+        [   # Semana
+            {"Semana 1": "Nangatitza", "lunes": 30},
+            {"Semana 1": "Nangatitza", "Martes": 25},
+            {"Semana 1": "Nangatitza", "Miercoles": 28},
+            {"Semana 1": "Nangatitza", "Jueves": 30},
+            {"Semana 1": "Nangatitza", "Viernes": 26},
+            {"Semana 1": "Nangatitza", "Sabado": 25},
+            {"Semana 1": "Nangatitza", "Domingo": 29}
+        ],
+        [   # Semana 2
+            {"Semana 2": "Nangatitza", "lunes": 30},
+            {"Semana 2": "Nangatitza", "Martes": 27},
+            {"Semana 2": "Nangatitza", "Miercoles": 30},
+            {"Semana 2": "Nangatitza", "Jueves": 26},
+            {"Semana 2": "Nangatitza", "Viernes": 27},
+            {"Semana 2": "Nangatitza", "Sabado": 28},
+            {"Semana 2": "Nangatitza", "Domingo": 25}
+        ],
+        [   # Semana 3
+            {"Semana 3": "Nangatitza", "lunes": 25},
+            {"Semana 3": "Nangatitza", "Martes": 29},
+            {"Semana 3": "Nangatitza", "Miercoles": 27},
+            {"Semana 3": "Nangatitza", "Jueves": 29},
+            {"Semana 3": "Nangatitza", "Viernes": 28},
+            {"Semana 3": "Nangatitza", "Sabado": 27},
+            {"Semana 3": "Nangatitza", "Domingo": 28}
+        ],
+        [   # Semana 4
+            {"Semana 4": "Nangatitza", "lunes": 25},
+            {"Semana 4": "Nangatitza", "Martes": 29},
+            {"Semana 4": "Nangatitza", "Miercoles": 30},
+            {"Semana 4": "Nangatitza", "Jueves": 35},
+            {"Semana 4": "Nangatitza", "Viernes": 28},
+            {"Semana 4": "Nangatitza", "Sabado": 37},
+            {"Semana 4": "Nangatitza", "Domingo": 30}
+        ]
+    ],
+    [   # Ciudad 3
+        [   # Semana 1
+            {"Semana 1": "Zumbi", "lunes": 29},
+            {"Semana 1": "Zumbi", "Martes": 25},
+            {"Semana 1": "Zumbi", "Miercoles": 35},
+            {"Semana 1": "Zumbi", "Jueves": 39},
+            {"Semana 1": "Zumbi", "Viernes": 30},
+            {"Semana 1": "Zumbi", "Sabado": 28},
+            {"Semana 1": "Zumbi", "Domingo": 29}
+        ],
+        [   # Semana 2
+            {"Semana 2": "Zumbi", "lunes": 25},
+            {"Semana 2": "Zumbi", "Martes": 30},
+            {"Semana 2": "Zumbi", "Miercoles": 36},
+            {"Semana 2": "Zumbi", "Jueves": 30},
+            {"Semana 2": "Zumbi", "Viernes": 33},
+            {"Semana 2": "Zumbi", "Sabado": 29},
+            {"Semana 2": "Zumbi", "Domingo": 30}
+        ],
+        [   # Semana 3
+            {"Semana 3": "Zumbi", "lunes": 28},
+            {"Semana 3": "Zumbi", "Martes": 26},
+            {"Semana 3": "Zumbi", "Miercoles": 24},
+            {"Semana 3": "Zumbi", "Jueves": 29},
+            {"Semana 3": "Zumbi", "Viernes": 30},
+            {"Semana 3": "Zumbi", "Sabado": 33},
+            {"Semana 3": "Zumbi", "Domingo": 37}
+        ],
+        [   # Semana 4
+            {"Semana 4": "Zumbi", "lunes": 27},
+            {"Semana 4": "Zumbi", "Martes": 26},
+            {"Semana 4": "Zumbi", "Miercoles": 30},
+            {"Semana 4": "Zumbi", "Jueves": 36},
+            {"Semana 4": "Zumbi", "Viernes": 26},
+            {"Semana 4": "Zumbi", "Sabado": 33},
+            {"Semana 4": "Zumbi", "Domingo": 36}
+        ]
+    ]
+]
+
+
+for ciudad in range(len(matr_temp)):#Bucles for anidados para recorrer los elementos
+    suma_mes =[]
+    for semana in range(len(matr_temp[ciudad])):
+        suma = 0
+        for dia in range(len(matr_temp[ciudad][semana])):
+            for i in matr_temp [ciudad][semana][dia]:
+                if i in range (0,100):
+                     suma += i
+                     suma_prom = suma/7
+            if 'Zamora' in matr_temp[ciudad][semana][dia][1]:
+                    suma_mes.append(suma_prom)
+                     print(f' El promedio de la semana {semana+1} es: {suma_prom:.2f} para la ciudad de ', 'Zamora')
+                if len(suma_mes)== 4:
+                    suma_mes_prom = sum(suma_mes)/4
+                    print(f' El promedio de la temperatura en Zamora en este mes fue de {suma_mes_prom:.2f}')
+            if 'Nangaritza' in matr_temp[ciudad][semana][dia][1]:
+                    suma_mes.append(suma_prom)
+                    print(f' El promedio de la semana {semana+1} es: {suma_prom:.2f} para la ciudad de ', 'Nangaritza')
+                    if len(suma_mes)== 4:
+                        suma_mes_prom = sum(suma_mes)/4
+                        print(f'El promedio de la temperatura Nangatitza en este mes fue de {suma_mes_prom:.2f}')
+            if 'Zumbi'in matr_temp[ciudad][semana][dia][1]:
+                suma_mes.append(suma_prom)
+                print(f' El promedio de la semana {semana+1} es:{suma_prom:.2f} para la ciudad de',  'Zumbi')
+                if len (suma_mes)== 4:
+                    suma_mes_prom = sum(suma_mes) / 4
+                print(f' El promedio de la temperatura Zumbi en este mes fue de {suma_prom:.2f}')
+
